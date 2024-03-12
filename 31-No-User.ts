4e600576704c45db6bdce6e62946e46ef94070ff
Bilal-Gulzar <163137250+Bilal-Gulzar@users.noreply.github.com>
2024-03-13 01:35:56 +0500

    Add files via upload

---
+++ 31-No-User.ts
@@ -0,0 +1,23 @@
+// Exercise No : 31 NO User:
+
+let userNames: string[] = [
+  "Osama",
+  "Abdulraffay",
+  "Admin",
+  "Sajid",
+  "Umer",
+  "Ali",
+];
+
+do {
+        userNames = []
+
+       if (userNames.length === 0) {
+          console.log("we need to find some users!.\n\n");
+          console.log("All users in Array. " + userNames.length);
+  } 
+
+    
+} 
+
+while (userNames.length > 0);
