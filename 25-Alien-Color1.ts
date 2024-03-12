746cf61ed45b3c935b7e741c033ce1fd37b477a6
Bilal-Gulzar <163137250+Bilal-Gulzar@users.noreply.github.com>
2024-03-13 01:26:58 +0500

    Add files via upload

---
+++ 25-Alien-Color1.ts
@@ -0,0 +1,12 @@
+//Exercise No : 25 Alien Colors #1
+let  alien_color: string = "green";
+
+if(alien_color === "green"){
+    console.log( " player just earned 5 points.");
+}
+
+alien_color = "yellow";
+
+if(alien_color === "green"){
+    console.log( " player just earned 5 points.");
+}
