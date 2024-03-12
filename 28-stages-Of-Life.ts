43f92e0121da64740f9c8161c5adf500ac05a8ae
Bilal-Gulzar <163137250+Bilal-Gulzar@users.noreply.github.com>
2024-03-13 01:28:55 +0500

    Add files via upload

---
+++ 28-stages-Of-Life.ts
@@ -0,0 +1,33 @@
+//Exercise No : 28 Stages Of Life
+
+let age: number = 19;
+
+if(age < 2){
+
+    console.log("you are a Baby.");
+}
+
+else if(age >= 2 && age < 4){
+
+    console.log("you are a Toddler.");
+}
+
+else if(age >= 4 && age < 13){
+
+    console.log("you are a Kid.");
+}
+
+else if(age >= 13 && age < 20){
+
+    console.log("you are a Teenager.");
+}
+
+else if(age >= 20 && age < 65){
+
+    console.log("you are an Adult");
+}
+
+else{
+
+    console.log("you are an Elder");
+}
