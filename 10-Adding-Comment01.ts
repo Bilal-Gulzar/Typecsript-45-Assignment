cdf7183d5069a3e21690ae71444b5779196a17f7
Bilal-Gulzar <163137250+Bilal-Gulzar@users.noreply.github.com>
2024-03-13 01:13:44 +0500

    Add files via upload

---
+++ 10-Adding-Comment01.ts
@@ -0,0 +1,8 @@
+// this program written by Bilal 
+// Current Date : 26 february 2024
+//this program shows famous quotation That  was said by Albert einstein
+
+const person: string = "Albert Eintein"
+const Quote: string = "If you want to live a happy life , tie it to a goal, not to people or things"
+const famous_quotation: string = person +" Said " + Quote;
+console.log(famous_quotation);
