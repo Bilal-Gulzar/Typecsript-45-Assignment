51f238f554e8439c1e2f70b7a54f41ab8259d845
Bilal-Gulzar <163137250+Bilal-Gulzar@users.noreply.github.com>
2024-03-13 01:15:08 +0500

    Add files via upload

---
+++ 11.Names.ts
@@ -0,0 +1,12 @@
+const names =["Osama" ,"Abdulraffay" ,"Bilalwal" ,"Daniyal" ,"taha"];
+const name1 = names[0];
+const name2 = names[1];
+const name3 = names[2];
+const name4 = names[3];
+const name5 = names[4];
+
+console.log(name1);
+console.log(name2);
+console.log(name3);
+console.log(name4);
+console.log(name5);
