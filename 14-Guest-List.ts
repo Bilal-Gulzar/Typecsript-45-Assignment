606c167ea8db1ef26bdd5a6069d4cb809f103086
Bilal-Gulzar <163137250+Bilal-Gulzar@users.noreply.github.com>
2024-03-13 01:17:21 +0500

    Add files via upload

---
+++ 14-Guest-List.ts
@@ -0,0 +1,6 @@
+let guestlist: string[] =["Osama", "Daniyal", "Taha"];
+
+for(let i=0; i<guestlist.length; i++){
+
+    console.log( "Dear Mr. "  + guestlist[i] + "\n\nIt is our pleasure to invite you in our party.\n\nThank You " );
+}
