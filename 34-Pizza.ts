bf7179e6ac9d2dc53bcac6edbded6aa032276a44
Bilal-Gulzar <163137250+Bilal-Gulzar@users.noreply.github.com>
2024-03-13 01:37:45 +0500

    Add files via upload

---
+++ 34-Pizza.ts
@@ -0,0 +1,17 @@
+   // Exercise No : 34 Pizzas
+
+
+   let favoritePizzas: string[] = ["Pizza marinara", "Neapolitan pizza", "pepperoni pizza" ,"Chicago Pizza"];
+      
+  
+    for(let i=0; i<favoritePizzas.length; i++){
+
+      console.log(`I like ${favoritePizzas[i]}\n\n`)
+
+
+}
+
+    console.log(
+      "Pizza is one of my favorite dishes.I like chicago pizza. Pizza is a worldwide favorite. The reason for pizza spread is   that you can add anything to it and eat it anytime of the day and anywhere. The pizza originated in  Italy and it is one of the favorite foods for adults and children.\n\n I really love Pizza!"
+
+    );
