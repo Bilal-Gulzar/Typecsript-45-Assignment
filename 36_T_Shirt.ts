da07dffe7401915957df68e5594af0b97ba1f032
Bilal-Gulzar <163137250+Bilal-Gulzar@users.noreply.github.com>
2024-03-13 01:38:58 +0500

    Add files via upload

---
+++ 36_T_Shirt.ts
@@ -0,0 +1,13 @@
+// Exercise No : 36 T-Shirt.
+
+function make_shirt(size:string, message:string): void{
+
+    //""Summarize the shirt that's going to be made""
+
+      console.log(`\nI'm going to make a ${size} t-shirt.`);
+       console.log(`I will say, "${message}"`);
+}
+
+
+     // Call the fuction :
+     make_shirt("medium", "Do what you do best!.");
