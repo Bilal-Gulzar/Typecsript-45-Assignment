5e32e0e2d4a8d63217639d2a146c48b6fc725ed9
Bilal-Gulzar <163137250+Bilal-Gulzar@users.noreply.github.com>
2024-03-13 01:25:23 +0500

    Add files via upload

---
+++ 23-Conditional-Test.ts
@@ -0,0 +1,82 @@
+// Exercise No : 23  Conditional Test 
+
+// first test :
+
+let car: string = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru');
+
+// second test :
+
+let Bike: string = 'Honda';
+
+console.log("Is Bike === 'Honda'? I predict True.");
+
+console.log(Bike === 'Honda');
+
+// Third test :
+
+let  laptop: string = 'lenovo';
+
+console.log("Is laptop !== 'Dell'? I predict True.")
+
+console.log(laptop !== 'Dell');
+
+// Fourth test :
+
+let orange: string = 'sour';
+
+console.log("Is orange === 'sour'? I predict True.")
+
+console.log(orange === 'sour');
+
+// fiveth test :
+
+let mangoColor: string = 'yellow';
+
+console.log("Is mangoColor === 'yellow'? I predict True.");
+
+console.log(mangoColor === 'yellow');
+
+
+//  sixth test :
+
+let richMan: string = 'elon musk';
+
+console.log("Is richMan == 'bill gate'? I predict False.");
+
+console.log(car == 'bill gate');
+
+// seventh test :
+
+let Car: string = 'subaru';
+
+console.log("Is Car == 'sabaru'? I predict True.");
+
+console.log(Car == 'sabaru');
+
+// eight test :
+
+let  bike: string = 'Hyundai';
+
+console.log("Is bike == 'Honda? I predict False.");
+
+console.log(bike === 'Honda');
+
+//  9 test :
+
+let strawberry: string = 'sour';
+
+console.log("Is strawberry !== 'sour'? I predict True.");
+
+console.log(strawberry !== 'sour');
+
+// 10 test :
+
+let caR = 'subaru';
+
+console.log("Is caR !== 'subaru'? I predict True.");
+
+console.log(caR !== 'subaru');
