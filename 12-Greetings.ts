52c2ed796991ea8457daa7ad10c3086f34b2b9b1
Bilal-Gulzar <163137250+Bilal-Gulzar@users.noreply.github.com>
2024-03-13 01:15:40 +0500

    Add files via upload

---
+++ 12-Greetings.ts
@@ -0,0 +1,14 @@
+const names =["Osama" ,"Abdulraffay" ,"Bilalwal" ,"Daniyal" ,"taha"];
+const name1 = names[0];
+const name2 = names[1];
+const name3 = names[2];
+const name4 = names[3];
+const name5 = names[4];
+const message: string = "Welcome to typescript class: ";
+
+console.log(message + name1);
+console.log(message + name2);
+console.log(message + name3);
+console.log(message + name4);
+console.log(message + name5);
+ 
