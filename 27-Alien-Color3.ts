a2061509178aae635c60f7112bfdf14dbc58ffae
Bilal-Gulzar <163137250+Bilal-Gulzar@users.noreply.github.com>
2024-03-13 01:28:24 +0500

    Add files via upload

---
+++ 27-Alien-Color3.ts
@@ -0,0 +1,66 @@
+//Exercise No: 27 Alien color 
+
+//Version of program #1
+
+let alienColor: string = "green";
+
+if(alienColor === "green"){
+
+    console.log("the player earned 5 points.");
+}
+else if (alienColor === "yellow"){
+
+    console.log(" the player earned 10 points.");
+}
+else if(alienColor === "red"){
+
+    console.log("player earned 15 points.");
+}
+else{
+
+    console.log(" you chose invalid Color ");
+}
+
+// Version  of program #2 
+
+alienColor = "yellow";
+
+
+if(alienColor === "green"){
+
+    console.log("the player earned 5 points.");
+}
+else if (alienColor === "yellow"){
+
+    console.log(" the player earned 10 points.");
+}
+else if(alienColor === "red"){
+
+    console.log("player earned 15 points.");
+}
+else{
+
+    console.log(" you chose invalid Color ");
+}
+
+//Version of program #3
+
+alienColor = "red";
+
+
+if(alienColor === "green"){
+
+    console.log("the player earned 5 points.");
+}
+else if (alienColor === "yellow"){
+
+    console.log(" the player earned 10 points.");
+}
+else if(alienColor === "red"){
+
+    console.log("player earned 15 points.");
+}
+else{
+
+    console.log(" you chose invalid Color ");
+}
