bc3c506ee659b442e0e731fba785b6890b879e7e
Bilal-Gulzar <163137250+Bilal-Gulzar@users.noreply.github.com>
2024-03-13 01:24:56 +0500

    Add files via upload

---
+++ 22-Intentional-Error.ts
@@ -0,0 +1,14 @@
+// Exercise No : 22 Try to make an array index error
+
+
+let intentionalError: string[] = ["Romain reigns", "the rock " ,"seth rolllins" ] 
+
+// making an array index error
+
+console.log( intentionalError[3]);
+
+//correcting  an array index error 
+
+intentionalError[3] = "cody rhodes"
+
+console.log( intentionalError[3]);
