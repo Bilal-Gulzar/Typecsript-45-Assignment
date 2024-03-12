25742158028834482516e7fecc6be361684f3b4c
Bilal-Gulzar <163137250+Bilal-Gulzar@users.noreply.github.com>
2024-03-13 01:29:21 +0500

    Add files via upload

---
+++ 29-Favorite-Fruits.ts
@@ -0,0 +1,25 @@
+// Exercise No : 29 Favorite Fruits
+
+const FavoriteFruits: string[] = ["Mango", "Banana", "Dates"];
+
+if(FavoriteFruits.includes("Mango")){
+
+    console.log("I really like Mango");
+}
+if(FavoriteFruits.includes("Banana")){
+
+console.log("I really like Banana");
+}
+
+if(FavoriteFruits.includes("Dates")){
+
+    console.log("I really like Dates");
+}
+if(FavoriteFruits.includes("strawberry")){
+
+    console.log("I really like Strawberry");
+}
+if(FavoriteFruits.includes("Apple")){
+
+    console.log("I really like Apple");
+}
