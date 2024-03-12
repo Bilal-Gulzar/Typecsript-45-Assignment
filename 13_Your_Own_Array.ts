b3299149a9dc9cc21fe0f8f24914b983e0a882ce
Bilal-Gulzar <163137250+Bilal-Gulzar@users.noreply.github.com>
2024-03-13 01:16:10 +0500

    Add files via upload

---
+++ 13_Your_Own_Array.ts
@@ -0,0 +1,7 @@
+let vehicles: string[] = ["car", "privatjat", " Honda motorcycle", "train "];
+
+for (let i =0; i<vehicles.length; i++){
+    
+    console.log("I would like to own a "  +vehicles[i] );
+
+}
