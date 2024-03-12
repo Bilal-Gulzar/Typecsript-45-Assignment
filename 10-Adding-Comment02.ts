3d74350379a9f7294df7f323607365a1cf6cee42
Bilal-Gulzar <163137250+Bilal-Gulzar@users.noreply.github.com>
2024-03-13 01:14:30 +0500

    Add files via upload

---
+++ 10-Adding-Comment02.ts
@@ -0,0 +1,7 @@
+//This program written by Bilal
+//Current Date: 26 february 2024
+// this program shows hello world and addition of two digits
+
+console.log("Hello World");
+const add: number = 30 + 16;
+console.log(add);
