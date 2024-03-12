ae06926890b7c9aceee8d4fa8bb77592e1258238
Bilal-Gulzar <163137250+Bilal-Gulzar@users.noreply.github.com>
2024-03-12 23:24:11 +0500

    Delete 02-Personal-Message.ts

--- 02-Personal-Message.ts
+++
@@ -1,9 +0,0 @@
-let person_name : string = prompt("what is your name ?") || ''
-
-if (person_name !== '' && person_name !== null){
-    alert(`Hello ${person_name}, would you like to learn some Python today?”`)
-}
-
-else {
-    alert("please you have to fill this ");
-}
